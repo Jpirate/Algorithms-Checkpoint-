d569058d2dfbb94c0e03639ad3857d6ccb85d902
Mohammed Fouda <mgamal243@gmail.com>
2021-07-18 16:29:42 +0200

    hash table

--- Linear Data St.js
+++ Linear Data St.js
@@ -16,10 +16,7 @@ for (let element of set1) {
     }
 }
 
-set1 = [3,1,7,9];
-set2 = [2,4,1,9,3];
-let is_unique;
-let uniqueArray = []
+
 for (let element of set2) {
     is_unique = true;
     for (let element2 of set1){
@@ -37,5 +34,25 @@ for (let element of set2) {
 var sum = 0;
 for (let element of uniqueArray) {
     sum += element; 
+}
+console.log (sum);
+
+
+// second solution 
+arr = [3,1,7,9,2,4,1,9,3];
+countObj = {};
+for (element of arr) {
+    if (countObj[element] == undefined){
+        countObj[element] = 1; 
+    }
+    else {
+        countObj[element] = countObj[element] + 1;
+    }    
+}
+sum = 0
+for (element in countObj) {
+    if (countObj[element] == 1) {
+      sum = sum + parseInt (element);
+    }
 }
 console.log (sum);
